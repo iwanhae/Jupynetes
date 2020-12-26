62476aafc32ee7d576cce0190c9646e7885cb287
wonpyoJang <jangwonpyo0@gmail.com>
2020-12-26 19:58:31 +0900

    added instance view to dashbaord

--- client/web/src/views/Dashboard/Dashboard.js
+++ client/web/src/views/Dashboard/Dashboard.js
@@ -14,16 +14,7 @@ import CardFooter from "components/Card/CardFooter.js";
 import Container from '@material-ui/core/Container';
 
 import Box from "@material-ui/core/Box"
-import purple from '@material-ui/core/colors/purple';
-
-import { bugs, website, server } from "variables/general.js";
 import Typography from '@material-ui/core/Typography';
-
-import {
-  dailySalesChart,
-  emailsSubscriptionChart,
-  completedTasksChart
-} from "variables/charts.js";
 
 import Button from "components/CustomButtons/Button.js";
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
@@ -32,130 +23,94 @@ const useStyles = makeStyles(styles);
 
 export default function Dashboard() {
   const classes = useStyles();
+
+  const statusStyle={
+    height: "50px",
+    width: "50px",
+    backgroundColor: "#F00",
+    borderRadius: "50%",
+    display: "inline-block",
+  }
+
+  const deleteButtonStyle={
+    height: "50px",
+    width: "50px",
+    backgroundColor: "#992222",
+  }
+
+  const accessButtonStyle={
+    height: "50px",
+    width: "50px",
+    backgroundColor: "#009955",
+  }
+
+
   return (
     <div>
       <GridContainer>
         <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <Box color="warning">
-              <CardHeader color="success" stats icon>
-                <Button type="button" color="info"><h3>사과서버</h3></Button>
-              </CardHeader>
-            </Box>
-            <Container>
-             <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '22vh' }}>
-              <CardBody>
-                <p>언제 생성</p>
-                <p>몇일째 구동중</p>
-                <p>각종 잡다한 세부정보</p>
-                <p>구동중인 이미지</p>
-              </CardBody> 
-             </Typography>
-            </Container>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div>
-            </CardFooter>
-          </Card>
+          {Instace(statusStyle, deleteButtonStyle, accessButtonStyle, classes)}
         </GridItem>
-                <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <h3 className={classes.cardTitle}>사과서버</h3>
-            </CardHeader>
-            <CardBody>
-              <p>언제 생성</p>
-              <p>몇일째 구동중</p>
-              <p>각종 잡다한 세부정보</p>
-              <p>구동중인 이미지</p>
-            </CardBody>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div>
-            </CardFooter>
-          </Card>
+        <GridItem xs={12} sm={6} md={3}>
+          {Instace(statusStyle, deleteButtonStyle, accessButtonStyle, classes)}
         </GridItem>
-                <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <h3 className={classes.cardTitle}>사과서버</h3>
-            </CardHeader>
-            <CardBody>
-              <p>언제 생성</p>
-              <p>몇일째 구동중</p>
-              <p>각종 잡다한 세부정보</p>
-              <p>구동중인 이미지</p>
-            </CardBody>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div>
-            </CardFooter>
-          </Card>
+        <GridItem xs={12} sm={6} md={3}>
+          {Instace(statusStyle, deleteButtonStyle, accessButtonStyle, classes)}
         </GridItem>
-                <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <h3 className={classes.cardTitle}>사과서버</h3>
-            </CardHeader>
-            <CardBody>
-              <p>언제 생성</p>
-              <p>몇일째 구동중</p>
-              <p>각종 잡다한 세부정보</p>
-              <p>구동중인 이미지</p>
-            </CardBody>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div>
-            </CardFooter>
-          </Card>
+        <GridItem xs={12} sm={6} md={3}>
+          {Instace(statusStyle, deleteButtonStyle, accessButtonStyle, classes)}
         </GridItem>
-                <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <h3 className={classes.cardTitle}>사과서버</h3>
-            </CardHeader>
-            <CardBody>
-              <p>언제 생성</p>
-              <p>몇일째 구동중</p>
-              <p>각종 잡다한 세부정보</p>
-              <p>구동중인 이미지</p>
-            </CardBody>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div>
-            </CardFooter>
-          </Card>
+        <GridItem xs={12} sm={6} md={3}>
+          {Instace(statusStyle, deleteButtonStyle, accessButtonStyle, classes)}
         </GridItem>
-                <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <h3 className={classes.cardTitle}>사과서버</h3>
-            </CardHeader>
-            <CardBody>
-              <p>언제 생성</p>
-              <p>몇일째 구동중</p>
-              <p>각종 잡다한 세부정보</p>
-              <p>구동중인 이미지</p>
-            </CardBody>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                Just Updated
-              </div>
-            </CardFooter>
-          </Card>
+        <GridItem xs={12} sm={6} md={3}>
+          {Instace(statusStyle, deleteButtonStyle, accessButtonStyle, classes)}
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          {Instace(statusStyle, deleteButtonStyle, accessButtonStyle, classes)}
         </GridItem>
       </GridContainer>
     </div>
   );
 }
+function Instace(statusStyle, deleteButtonStyle, accessButtonStyle, classes) {
+  return <Card>
+    <Box color="warning">
+      <CardHeader color="success" stats icon>
+        <Button type="button" color="info"><h3>사과서버</h3></Button>
+      </CardHeader>
+    </Box>
+    <Container>
+      <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '22vh' }}>
+        <CardBody>
+          <p>언제 생성</p>
+          <p>몇일째 구동중</p>
+          <p>각종 잡다한 세부정보</p>
+          <p>구동중인 이미지</p>
+        </CardBody>
+      </Typography>
+      <GridContainer>
+        <GridItem xs={12} sm={6} md={5}>
+          <span class="dot" style={statusStyle}></span>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Button type="button" style={deleteButtonStyle}>
+            삭제
+          </Button>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Button type="button" style={accessButtonStyle}>
+                      접속
+          </Button>
+        </GridItem>
+      </GridContainer>
+    </Container>
+    <CardFooter stats>
+      <div className={classes.stats}>
+        <Update />
+        Just Updated
+      </div>
+    </CardFooter>
+  </Card>;
+}
+
